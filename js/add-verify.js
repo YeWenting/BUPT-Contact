53f4af8a301a421e25652cacdfc1012ee79acb37
Wenting Ye <Wenting_Ye@Outlook.com>
2016-11-14 19:57:58 +0800

    fix bug

--- js/add-verify.js
+++ js/add-verify.js
@@ -1,55 +1,11 @@
 /**
- * Created by YeWenting on 2016/11/13.
+ * Created by YeWenting on 14/11/2016.
  */
-
-function checkEmail(email)
+function editName(item)
 {
-    var emailPat=/^(.+)@(.+)$/;
-    var matchArray=email.match(emailPat);
-    if (matchArray==null) return false;
-    else return true;
-}
-
-function checkSignIn()
-{
-    var email = document.getElementById("signInEmail").value;
-    var password = document.getElementById("signInPass").value;
-
-    /* An easy email verify */
-    if (checkEmail(email))
-        alert("Email address is not valid! ");
-
-    /* An easy password check */
-    if (password.length < 8) alert("Password should be more than 8 characters!");
-    if (password.match(/([0-9])+/) == null) alert("Password should contain number!");
-    if (password.match(/([A-Za-z])+/) == null) alert("Password should contain English letter!");
-}
-
-function checkSignUp()
-{
-    var name = document.getElementById("nickname").value;
-    var email = document.getElementById("signUpEmail").value;
-    var password = document.getElementById("signUpPass").value;
-
-    /* An easy name verify */
-    if (name.length > 10) alert("Nickname is limited to 10 characters!");
-    if (name.match(/([A-Za-z])+/) == null) alert("Nickname should contain English letter!");
-
-    /* An easy email verify */
-    if (checkEmail(email))
-        alert("Email address is not valid! ");
-
-    /* An easy password check */
-    if (password.length < 8) alert("Password should be more than 8 characters!");
-    if (password.match(/([0-9])+/) == null) alert("Password should contain number!");
-    if (password.match(/([A-Za-z])+/) == null) alert("Password should contain English letter!");
-}
-
-function editName(item, warnid)
-{
-    warnid = "error-tab" + warnid;
+    var warnid = "error-tab"
     var wrong = false;
-    var name = item.innerHTML;
+    var name = item.value;
     var s = document.getElementById(warnid).innerHTML;
 
     /* Check the length */
@@ -71,21 +27,23 @@ function editName(item, warnid)
 
     if (wrong)
     {
-        item.innerHTML = "null";
+        item.value = "null";
         document.getElementById(warnid).parentNode.style.display = "inline";
         document.getElementById(warnid).innerHTML = s;
+        //document.getElementById("submit-button").setAttribute("disabled", "true");
     }
     else
     {
         document.getElementById(warnid).innerHTML = s;
         if (s == "") document.getElementById(warnid).parentNode.style.display = "none";
+        //document.getElementById("submit-button").setAttribute("disabled", "false");
     }
 }
 
-function editCellphone(item, warnid)
+function editCellphone(item)
 {
-    warnid = "error-tab" + warnid;
-    var phone = item.innerHTML.trim();
+    var warnid = "error-tab";
+    var phone = item.value.trim();
     var s = document.getElementById(warnid).innerHTML;
     var mes = "Cellphone number not valid! "
 
@@ -93,21 +51,23 @@ function editCellphone(item, warnid)
     if (phone.match(/(86)*0*13\d{9}/) == null)
     {
         if (s.search(mes) < 0) s += mes;
-        item.innerHTML = "null";
+        item.value = "null";
         document.getElementById(warnid).parentNode.style.display = "inline";
         document.getElementById(warnid).innerHTML = s;
+        //document.getElementById("submit-button").setAttribute("disabled", "true");
     }
     else {
         s = s.replace(mes, "");
         document.getElementById(warnid).innerHTML = s;
         if (s == "") document.getElementById(warnid).parentNode.style.display = "none";
+        //document.getElementById("submit-button").setAttribute("disabled", "false");
     }
 }
 
-function editTelephone(item, warnid)
+function editTelephone(item)
 {
-    warnid = "error-tab" + warnid;
-    var phone = item.innerHTML.trim();
+    var warnid = "error-tab";
+    var phone = item.value.trim();
     var s = document.getElementById(warnid).innerHTML;
     var mes = "Telephone number not valid! "
 
@@ -115,21 +75,23 @@ function editTelephone(item, warnid)
     if (phone.match(/\d{2}-\d{8}|\d{3}-\d{8}|\d{10}|\d{11}/) == null)
     {
         if (s.search(mes) < 0) s += mes;
-        item.innerHTML = "null";
+        item.value = "null";
         document.getElementById(warnid).parentNode.style.display = "inline";
         document.getElementById(warnid).innerHTML = s;
+        //document.getElementById("submit-button").setAttribute("disabled", "true");
     }
     else
     {
         s = s.replace(mes, "");
         document.getElementById(warnid).innerHTML = s;
         if (s == "") document.getElementById(warnid).parentNode.style.display = "none";
+        //document.getElementById("submit-button").setAttribute("disabled", "false");
     }
 }
-function editEmail(item, warnid)
+function editEmail(item)
 {
-    warnid = "error-tab" + warnid;
-    var email= item.innerHTML;
+    var warnid = "error-tab";
+    var email= item.value;
     var s = document.getElementById(warnid).innerHTML;
     var mes = "Email not valid! "
 
@@ -137,22 +99,24 @@ function editEmail(item, warnid)
     if (!checkEmail(email))
     {
         if (s.search(mes) < 0) s += mes;
-        item.innerHTML = "null";
+        item.value = "null";
         document.getElementById(warnid).parentNode.style.display = "inline";
         document.getElementById(warnid).innerHTML = s;
+        //document.getElementById("submit-button").setAttribute("disabled", "true");
     }
     else
     {
         s = s.replace(mes, "");
         document.getElementById(warnid).innerHTML = s;
         if (s == "") document.getElementById(warnid).parentNode.style.display = "none";
+        //document.getElementById("submit-button").setAttribute("disabled", "false");
     }
 }
 
-function editOICQ(item, warnid)
+function editOICQ(item)
 {
-    warnid = "error-tab" + warnid;
-    var qq= item.innerHTML;
+    var warnid = "error-tab";
+    var qq= item.value;
     var s = document.getElementById(warnid).innerHTML;
     var mes = "QQ not valid! "
 
@@ -160,14 +124,16 @@ function editOICQ(item, warnid)
     if (isNaN(qq))
     {
         if (s.search(mes) < 0) s += mes;
-        item.innerHTML = "null";
+        item.value = "null";
         document.getElementById(warnid).parentNode.style.display = "inline";
         document.getElementById(warnid).innerHTML = s;
+        //document.getElementById("submit-button").setAttribute("disabled", "true");
     }
     else
     {
         s = s.replace(mes, "");
         document.getElementById(warnid).innerHTML = s;
         if (s == "") document.getElementById(warnid).parentNode.style.display = "none";
+        //document.getElementById("submit-button").setAttribute("disabled", "false");
     }
 }
